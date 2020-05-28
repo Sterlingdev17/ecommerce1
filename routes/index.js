f492a4077974cafa0be2a580c1bea0c86187e1e4
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-05-28 11:08:15 -0400

    card

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ghiacciu' });
+  res.render('shop/index', { title: 'Ghiacciu' });
 });
 
 module.exports = router;
