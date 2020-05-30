cac49a8a7315b0fccb23560d987ad5c0d76db551
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-05-30 00:26:35 -0400

    change

--- README.md
+++
@@ -1,2 +0,0 @@
-# ecommerce1
-my ecommerce
---
+++ server.js
@@ -0,0 +1,27 @@
+require("./models/db");
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const orderController = require("./controllers/orderController");
+
+const app = express()
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"))
+app.engine("hbs",exphbs({
+    extname: "hbs",
+    defaultLayout: "mainLayout",
+    layoutsDir: __dirname+ "/views/"
+}))
+
+app.set("view engine", "hbs");
+app.listen(3000, ()=>{
+    console.log("server listening....... on port 3000");
+
+});
+
+app.use("/", orderController);
